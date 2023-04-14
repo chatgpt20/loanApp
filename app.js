bc6873339649d5250a0d90895048f2ab4f7e1241
yehanopatha <yehan.opatha@inivossl.com>
2023-04-14 13:05:45 +0530

    sign in page ui done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res) {
-  res.send("Hello World");
+  res.sendFile(__dirname + '/html/signin.html');
 })
 
 app.listen(3000, function() {
