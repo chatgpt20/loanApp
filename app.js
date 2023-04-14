aa832c7f2a42ffcd15c16f4c5bcc5b1dcec3132d
yehanopatha <yehan.opatha@inivossl.com>
2023-04-14 12:23:45 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res) {
+  res.send("Hello World");
+})
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
